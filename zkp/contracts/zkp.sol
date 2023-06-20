b364b4f9905d1ed4af49e858df634959cfeeadc1
FedericoValbusa <federico.valbusa@studenti.unitn.it>
2023-06-20 16:39:00 +0200

    Authentication and verification added
    na fracca de roba

--- zkp/contracts/zkp.sol
+++ zkp/contracts/zkp.sol
@@ -1,21 +1,23 @@
 // SPDX-License-Identifier: MIT
 // compiler version must be greater than or equal to 0.8.17 and less than 0.9.0
 pragma solidity ^0.8.17;
+import "./Secp256k1.sol";
 import "./EllipticCurve.sol";
 import "./EllipticCurveFastMult.sol";
+
 //import "hardhat/console.sol"; //used for logger
 //import "./SafeMath.sol";
 
 contract zkp{
-    uint256 public constant GX = 0x79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798;
-    uint256 public constant GY = 0x483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8;
-    uint256 public constant AA = 0;
-    uint256 public constant BB = 7;
-    uint256 public constant PP = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F;
-    uint256 public constant N = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141;
+    uint256 public GX = Secp256k1.getGX();
+    uint256 public GY = Secp256k1.getGY();
+    uint256 public AA = Secp256k1.getAA();
+    uint256 public BB = Secp256k1.getBB();
+    uint256 public PP = Secp256k1.getPP();
+    uint256 public N = Secp256k1.getN();
 
-    function Prover(uint256 x) public view returns /*(bytes20, uint256)*/ (uint256, uint256, uint256, uint256, uint256, uint256) {
-            uint256 r = uint256(keccak256(abi.encodePacked(block.difficulty, block.timestamp))) % N;
+    function Prover(uint256 x) public view returns (uint256, uint256, uint256, uint256, uint256, uint256) {
+            uint256 r = uint256(keccak256(abi.encodePacked(block.prevrandao, block.timestamp))) % N;
             
             (uint256 hx,uint256 hy) = EllipticCurve.ecMul(x, GX, GY, AA, PP);
             (uint256 ux,uint256 uy) = EllipticCurve.ecMul(r, GX, GY, AA, PP);
@@ -25,7 +27,7 @@ contract zkp{
             return (ux, uy, c256, z, hx, hy);
     }
 
-    function Verifier(uint256 ux, uint256 uy, uint256 c, uint256 z, uint256 hx, uint256 hy) public pure returns (bool){
+    function Verifier(uint256 ux, uint256 uy, uint256 c, uint256 z, uint256 hx, uint256 hy) public view returns (bool){
             (uint256 h_cx, uint256 h_cy) = EllipticCurve.ecMul(c, hx, hy, AA, PP);
             (uint256 point1_x, uint256 point1_y) =  EllipticCurve.ecMul(z, GX, GY, AA, PP);
             (uint256 point2_x, uint256 point2_y) = EllipticCurve.ecAdd(ux, uy, h_cx, h_cy, AA, PP);
